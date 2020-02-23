6e8c121e603b160dec25e71953f7f9819f834055
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2020-02-23 14:07:36 +0530

    React Boilers.

---
+++ README.md
@@ -0,0 +1,10 @@
+A simplified version of create-react-app. Help to manage and write clean code
+
+To start a server
+
+### npm start
+
+To create Production Build 
+
+### npm run build
+
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Stylize.css';
+import Nav from './components/Navbar/Nav'
+
+function App() {
+  return (
+    <>
+    <Nav />
+    </>
+  );
+}
+
+export default App;
